c9357c559be83d8a582c0a143a512bda8f566830
Carl Sachs <carlton@Carls-MacBook-Air.local>
2020-07-01 23:24:03 -0400

    changing things up with text on homepage

--- portfolio/src/components/HomeTop.js
+++ portfolio/src/components/HomeTop.js
@@ -5,17 +5,18 @@ import "../App.css";
 
 //import styling libraries
 import Fade from "react-reveal";
+import Bounce from "react-reveal/Bounce";
 import Typist from "react-typist";
 
 
 const HomeTop = () => {
 
     return (
-        <Fade bottom>
+        <Fade top>
         <div className="topdiv">
-            <div>
-
-            </div>
+                    <div className="title">
+                        <h1 aria-label="../../Carl/Sachs"></h1>
+                    </div>
         </div>
         </Fade>
     )
