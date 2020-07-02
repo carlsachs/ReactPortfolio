3cc21a26859d16b5356dc85f790ab203291503cd
Carl Sachs <carlton@Carls-MacBook-Air.local>
2020-07-02 01:12:06 -0400

    done for the night

--- portfolio/src/components/UnderTop.js
+++ portfolio/src/components/UnderTop.js
@@ -10,12 +10,26 @@ import Fade from "react-reveal";
 const UnderTop = () => {
 
     return (
-        <div className="Wrap">
+        <div className="wrap">
             <Fade top>
                 <h1>Skills</h1>
             </Fade>
-            <div className="content">
-                
+            <div className="boxes">
+                <Fade left>
+                    <div className="box1">
+                        <h2>Front-End</h2>
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div className="box2">
+                        <h2>Back-End</h2>
+                    </div>
+                </Fade>
+                <Fade right>
+                    <div className="box3">
+                        <h2>Computer Science</h2>
+                    </div>
+                </Fade>
             </div>
         </div>
     )
