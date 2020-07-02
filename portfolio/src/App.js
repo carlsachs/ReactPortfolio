7d53c55d6e38a987d8b10deea85289e5accd0b7f
Carl Sachs <carlton@Carls-MacBook-Air.local>
2020-07-01 20:24:56 -0400

    initial setup done, messing around with creativity and ideas

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+//import components
+import HomeTop from "../src/components/HomeTop";
+
+function App() {
+  return (
+    <HomeTop />
+  )
+}
+
+export default App;
---
+++ portfolio/src/components/HomeTop.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+//import styling files
+import "../App.css";
+
+//import styling libraries
+import Fade from "react-reveal";
+import Typist from "react-typist";
+
+
+const HomeTop = () => {
+
+    return (
+        <Fade bottom>
+        <div className="topdiv">
+            <div>
+
+            </div>
+        </div>
+        </Fade>
+    )
+}
+
+export default HomeTop;
